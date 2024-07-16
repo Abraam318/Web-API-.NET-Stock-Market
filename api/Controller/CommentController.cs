1e9ce0c0c101dc1d40a3c8ee91eab7034d330099
Abraam Adel Demitry <126262425+Abraam318@users.noreply.github.com>
2024-07-16 17:04:48 +0300

    Get All Comments

---
+++ api/Controller/CommentController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Interfaces;
+using api.Mapper;
+using api.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controller
+{
+    [Route("api/[controller]")] 
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentRepository _commentRepo;
+        public CommentController(ICommentRepository commentRepo)
+        {
+            _commentRepo = commentRepo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll(){
+            var comment = await _commentRepo.GetAllCommentsAsync();
+            var commentDto = comment.Select(s=>s.TocommentDto());
+            return Ok(commentDto);
+        }
+    }
+}
---
+++ api/Mapper/CommentMapper.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos;
+using api.Models;
+
+namespace api.Mapper
+{
+    public static class CommentMapper
+    {
+        public static CommentDto TocommentDto(this Comment commentModel){
+            return new CommentDto{
+                Id = commentModel.Id,
+                Title = commentModel.Title,
+                Content = commentModel.Content,
+                CreatedOn =commentModel.CreatedOn.Date,
+                StockId = commentModel.StockId
+            };
+        }
+    }
+}
